90bd3b968b4c4a602c3ecc948199c4ccd08f7b05
Biplop Giri <85874935+B33pl0p@users.noreply.github.com>
2025-02-01 15:03:17 +0545

    Update categories grid to match with database categories

--- src/app/components/categories.jsx
+++ src/app/components/categories.jsx
@@ -1,12 +1,11 @@
-import { FaTshirt, FaCar, FaCouch, FaMicrochip, FaMusic, FaSmile, FaBook, FaShoePrints, FaGift, FaDumbbell } from "react-icons/fa";
+import { FaTshirt, FaCar, FaCouch, FaMicrochip, FaMusic, FaSmile, FaBook, FaShoePrints, FaGift, FaDumbbell,FaGlasses } from "react-icons/fa";
 import { MdOutlineCleaningServices } from "react-icons/md";
 
 const categories = [
   { id: "1", name: "Apparel", icon: <FaTshirt /> },
-  { id: "2", name: "Accessories", icon: <FaTshirt /> },
+  { id: "2", name: "Accessories", icon: <FaGlasses /> },
   { id: "3", name: "Automobiles", icon: <FaCar /> },
   { id: "4", name: "Furnishing", icon: <FaCouch /> },
-  { id: "5", name: "Apparels", icon: <FaTshirt /> },
   { id: "6", name: "Electronics", icon: <FaMicrochip /> },
   { id: "7", name: "Musical", icon: <FaMusic /> },
   { id: "8", name: "Beauty", icon: <FaSmile /> },
